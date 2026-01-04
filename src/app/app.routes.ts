b37e66b379cbb7e563547bde95964322f7eaf3f7
Pieter Goyens <pietergoyens@hotmail.com>
2026-01-04 14:07:42 +0100

    Basic routing setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { Login } from './features/login/login';
+import { NotFound } from './features/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home, },
+
+    { path: 'login', component: Login },
+
+    { path: '**', component: NotFound }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,15 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterLink, RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css',
+  styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('hvz-oost');
-
+  title = 'Hulpverleningszone Oost Vlaams-Brabant: post Landen';
+  footerUrl = 'https://oost-vlaams-brabant.hulpverleningszone.be';
+  footerLink = 'oost-vlaams-brabant.hulpverleningszone.be';
 }
